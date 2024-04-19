3b9ebde3e1166f02b4ad176f04a63d53744ff288
wws <wws741@qq.com>
2024-04-19 22:16:49 +0800

    0419

--- preload.js
+++ preload.js
@@ -1,9 +1,18 @@
-window.addEventListener('DOMContentLoaded',()=>{
-    const replaceText=(selector,text)=>{
-        const element=document.getElementById(selector)
-        if (element) element.innerText=text
-    }
-    for (const dependency of ['chrome','node','electron']){
-        replaceText(`${dependency}-version`,process.versions[dependency])
-    }
+// window.addEventListener('DOMContentLoaded',()=>{
+//     const replaceText=(selector,text)=>{
+//         const element=document.getElementById(selector)
+//         if (element) element.innerText=text
+//     }
+//     for (const dependency of ['chrome','node','electron']){
+//         replaceText(`${dependency}-version`,process.versions[dependency])
+//     }
+// })
+
+const { contextBridge } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron
+  // 除函数之外，我们也可以暴露变量
 })
